b364924ad04948c814443756f4fff8a827237c55
Slim <slim.khadhraoui@gmail.com>
2020-11-23 16:00:03 -0500

    Added twitter lib to get the tweets cleanup

--- server/server.ts
+++ server/server.ts
@@ -1,26 +1,23 @@
 import * as express from 'express';
-import * as util from 'util';
 import * as bodyParser from 'body-parser';
 import * as http from 'http';
 import * as path from 'path';
+import { getTweets, Tweet } from './tweets';
 
 const app = express();
 const port = process.env.PORT || 3000;
-const request = require('request');
-const post = util.promisify(request.post);
-const get = util.promisify(request.get);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const server = http.createServer(app);
-// const socketIo = require('socket.io');
-// const io = socketIo(server);
-
-// const BEARER_TOKEN = process.env.TWITTER_BEARER_TOKEN;
 
 app.use(express.static(path.join(__dirname, '../client/dist/')));
 app.use('/dashboard', express.static(path.join(__dirname, '../client/dist/')));
 
+app.get('/tweets/:politician', async (req, res) => {
+    const tweets: Tweet[] = await getTweets('realDonaldTrump');
+    res.send(tweets);
+});
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ server/tweets.ts
@@ -0,0 +1,21 @@
+const Twitter = require('twitter');
+
+export interface Tweet {
+    id: string;
+    text?: string;
+}
+
+const twitterClient = new Twitter({
+    consumer_key: process.env.TWITTER_API_KEY || '',
+    consumer_secret: process.env.TWITTER_API_SECRET_KEY || '',
+    access_token_key: process.env.TWITTER_TOKEN_KEY || '',
+    access_token_secret: process.env.TWITTER_TOKEN_SECRET || '',
+});
+
+export const getTweets = async (politician: string): Promise<Tweet[]> => {
+    const params = {
+        screen_name: politician,
+        count: process.env.TWITTER_COUNT || 20,
+    };
+    return twitterClient.get('statuses/user_timeline', params);
+};
